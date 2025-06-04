91344a4dbf7cca14be19ac37fe8b506e6bd3f756
Owen <juanmig134187@gmail.com>
2025-06-04 19:55:30 +0800

    gui_for_name_ID_entry_

--- main_program_caller.py
+++ main_program_caller.py
@@ -18,3 +18,13 @@ class StudentApplication:
         self.manager = RecordManager()
         self.root = root
         self.root.title = ("Student Records Management Office")
+
+
+        self.id_entry = tk.Entry(root)
+        self.name_entry = tk.Entry(root)
+        self.grade_entry = tk.Entry(root)
+
+        tk.Label(root, text="Student ID").grid(rows=0, column=0)
+        self.id_entry.grid(row=0, column=1)
+        tk.Label(root, text="Name").grid(row=1, column=0)
+        self.name_entry.grid(row=1 , column=1)
